13870a224a4a0f3816919d7ee0c314bb754d0e81
Assaad MOAWAD <assaad.moawad@uni.lu>
2017-03-21 11:03:52 +0100

    Samples update

--- src/main/java/greycat/samples/ManyWorld.java
+++ src/main/java/greycat/samples/ManyWorld.java
@@ -19,16 +19,28 @@ public class ManyWorld {
 
             long world_1 = g.fork(world_0);
 
-            g.lookup(world_1, timepoint_0, sensor0.id(), new Callback<Node>() {
+            sensor0.travelInWorld(world_1, new Callback<Node>() {
                 @Override
-                public void on(Node result) {
-                    
+                public void on(Node sensorW1T0) {
+                    sensorW1T0.set("value", Type.DOUBLE, 4.6);
+
+                    long timepoint_1=10;
+                    sensorW1T0.travel(world_0, timepoint_1, new Callback<Node>() {
+                        @Override
+                        public void on(Node sensorW0T1) {
+                            System.out.println(sensorW0T1); // print {"world":0,"time":10,"id":1,"id":"4494F","name":"sensor0","value":0.5}
+                            sensorW0T1.travel(world_1, timepoint_1, new Callback<Node>() {
+                                @Override
+                                public void on(Node sensorW1T1) {
+                                    System.out.println(sensorW1T1); //print {"world":1,"time":10,"id":1,"id":"4494F","name":"sensor0","value":4.6}
+
+                                }
+                            });
+                        }
+                    });
+
                 }
             });
-
-
-
-
 
 
         });
