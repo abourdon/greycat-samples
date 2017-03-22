f67f155459fb73e329dca74b034c48388a28da59
Assaad MOAWAD <assaad.moawad@uni.lu>
2017-03-22 11:35:32 +0100

    update persistance

--- src/main/java/greycat/samples/Persistance.java
+++ src/main/java/greycat/samples/Persistance.java
@@ -16,7 +16,7 @@ public class Persistance {
 
         Graph g = new GraphBuilder()
                 .withMemorySize(10000) //cache size before sync to disk
-                .withStorage(new LevelDBStorage("mwg_db"))
+                .withStorage(new LevelDBStorage("greycat_db")) //location to store on disc
                 .build();
         g.connect(isConnected -> {
 
@@ -26,11 +26,13 @@ public class Persistance {
 
             g.index(0, 0, "sensors", nodeIndex -> {
                 nodeIndex.addToIndex(sensor);
+                //save the database here.
                 g.save(saveResult -> {
                     g.index(0, System.currentTimeMillis(), "sensors", nodeIndex2 -> {
                         nodeIndex2.find(new Callback<Node[]>() {
                             @Override
                             public void on(Node[] result) {
+                                //Get all the nodes stored on disk, each execution will increase the list
                                 for (int i = 0; i < result.length; i++) {
                                     System.out.println("\t" + result[i].toString());
                                 }
@@ -40,7 +42,6 @@ public class Persistance {
                     });
                 });
             });
-
         });
 
     }
